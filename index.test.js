335d177303949e64c508f689d5737a7dbd0795ab
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-17 12:17:01 +0000

    write tests for board model

--- index.test.js
+++ index.test.js
@@ -69,21 +69,56 @@ describe('User, Board and Cheese models', () => {
     })
 
     // Board model tests
-    // test('can create a board', async () => {
-
-    // })
+    test('can create a board', async () => {
+        // create board
+        const board1 = await Board.create({
+            type: 'Regional French Cheese Board',
+            description: 'A plethera of cheeses from all over France',
+            rating: 4
+        });
+        // retreive board from db
+        const findBoard1 = await Board.findByPk(board1.id);
+        expect(findBoard1.type).toBe('Regional French Cheese Board');
+        expect(findBoard1.description).toBe('A plethera of cheeses from all over France');
+        expect(findBoard1.rating).toBe(4);
+    })
 
     // test('can find a board', async () => {
         
     // })
 
-    // test('can update a board', async () => {
-        
-    // })
+    test('can update a board', async () => {
+        // create board
+        const board2 = await Board.create({
+            type: 'Vegan Cheese Board',
+            description: 'A variety of vegan cheese made from ingredients like nuts, soy or coconut milk',
+            rating: 2
+        });
+        // update the board
+        await Board.update({ rating: 1 }, { where: { id: board2.id } });
+        // query the db for the updated board
+        const findBoard2 = await Board.findByPk(board2.id);
+        expect(findBoard2.type).toBe('Vegan Cheese Board');
+        expect(findBoard2.description).toBe('A variety of vegan cheese made from ingredients like nuts, soy or coconut milk');
+        expect(findBoard2.rating).toBe(1);
+    })
 
-    // test('can delete a board', async () => {
-        
-    // })
+    test('can delete a board', async () => {
+        // create board
+        const board3 = await Board.create({
+            type: 'Smoked Cheese Board',
+            description: 'This features smoked cheeses, such as smoked gouda and smoked cheddar',
+            rating: 4
+        });
+        // get the board from the db
+        let findBoard3 = await Board.findByPk(board3.id);
+        // delete board3
+        await Board.destroy({ where: { id: board3.id } });
+        // try to retreive board3 from the db
+        findBoard3 = await Board.findByPk(board3.id);
+        expect(findBoard3).toBeNull();
+
+    })
 
     // Cheese model tests
     // test('can create a cheese', async () => {
